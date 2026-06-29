6deb7ce5050e764c2badd9909b8db982692fd799
rcnovello <ronnicorrea@hotmail.com>
2026-06-29 17:48:43 -0300

    Update footer tech stack and simplify links
    Reworked the footer content to show the project stack details (NestJS, ReactJS, Oracle, and infrastructure tools) instead of the previous generic profile text. It also hides the Partners and privacy-policy sections and simplifies the copyright line by removing the hardcoded year and policy reference.

--- src/footer.tsx
+++ src/footer.tsx
@@ -5,14 +5,17 @@ function Footer() {
             <div className="row">
             <div className="col l6 s12">
             <h5 className="white-text">Perfil de Desenvolvimento</h5>
-                <p className="grey-text text-lighten-4">Uma estrutura de front-end responsiva moderna baseada em Material Design</p>
+                <p className="grey-text text-lighten-4">Backend - NestJs</p>
+                <p className="grey-text text-lighten-4">Frontend - ReactJs</p>
+                <p className="grey-text text-lighten-4">Banco de Dados - Oracle</p>
+                <p className="grey-text text-lighten-4">Infraestrutura - AWS, VmWare, Linux</p>
             </div>
-            <div className="col l3 s12">
+            {/* <div className="col l3 s12">
                 <h5 className="white-text">Parceiros</h5>
                 <ul>
                 <li><a className="white-text" href="https://www.linkedin.com/in/ronni-novello" target="_blank">Linkedin</a></li>              
                 </ul>
-            </div>
+            </div> */}
             <div className="col l3 s12">
                 <h5 className="white-text">Connect</h5>
                 <ul>
@@ -22,16 +25,16 @@ function Footer() {
             </div>
             
             </div>
-            <div className="row">
+            {/* <div className="row">
                 <div className="col">
                     <a className="white-text" target="_blank" href="./politica_privacidade"> Política de Privacidade e Proteção a Dados Pessoais  </a>
                 </div>
-            </div>
+            </div> */}
         </div>      
 
         <div className="footer-copyright">
             <div className="container center">
-            © RCNDev | Todos os direitos reservados 2024 | Política de Privacidade<a className="brown-text text-lighten-3" href="http://materializecss.com"></a>
+            © RCNDev | Todos os direitos reservados 
             </div>
         </div>
     </footer>
